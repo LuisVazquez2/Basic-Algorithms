169bef3be70f8c43e364b7a991e5e94d76e58afd
Luis Vazquez <luisito986.22@gmail.com>
2020-11-25 23:19:27 -0600

    maint

--- Binary Search.cc
+++ Binary Search.cc
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+void BinarySeacrh(vector<int>& current,int Findnumber){
+    int low = 0 , top = current.size()-1;
+}
 int main(){
     vector<int> arre{1, 2, 3, 4};
     return 0;
