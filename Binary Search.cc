a7b41ba2409a1c6cc9423734d3655bc26b8d67dc
Luis Vazquez <luisito986.22@gmail.com>
2020-11-25 23:23:05 -0600

    Add While

--- Binary Search.cc
+++ Binary Search.cc
@@ -2,6 +2,15 @@
 using namespace std;
 void BinarySeacrh(vector<int>& current,int Findnumber){
     int low = 0 , top = current.size()-1;
+    while(low<=top){
+        int mid = (low + top) / 2;
+        if(current[mid] == Findnumber){
+            cout << "Yes";
+            return;
+        }
+        if(current[low]>Findnumber)
+            top = low;
+    }
 }
 int main(){
     vector<int> arre{1, 2, 3, 4};
